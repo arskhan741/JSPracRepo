2739a9f927da2c073989ef5246183191d817d73a
Arsalan Khan <arskhanfrs@gmail.com>
2024-06-11 12:35:35 +0500

    String Methods done

---
+++ StringMethods/StringMethods.js
@@ -0,0 +1,26 @@
+let myName = "Harry";
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+let friendName = "Bob";
+
+console.log(myName.concat(" is a friend of " , friendName));
+
+
+let nonTrimmedString = "    abc         ";
+
+//Removes Spaces
+console.log(nonTrimmedString.trim());
+
+let fr = "Arsalan";
+
+// for(key in fr)
+//     {
+//         console.log(key);
+//     }
+
+for(let i=0; i<fr.length; i++)
+    {
+        console.log(fr[i]);
+    }
