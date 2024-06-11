6593df9995c13fb37b0a0ea0be4e5f3aec42e0aa
Arsalan Khan <arskhanfrs@gmail.com>
2024-06-11 11:39:07 +0500

    Test

---
+++ DataTypes/Types.js
@@ -0,0 +1,34 @@
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const oustideTemp = null;
+
+let userEmail;
+
+const bigNumber = 321321321321321321n;
+let imaginaryNumber;
+
+const heros = ["superman", "batman"];
+
+let myObj = {
+    name : "Arsalan",
+    age : 27
+};
+
+function myFunction() {
+    console.log(heros);
+}
+
+// console.log("type of imaginaryNumber = "+typeof(imaginaryNumber))
+// console.log("type of bigNumber = "+typeof(bigNumber));
+// console.log("type of scoreValue = "+typeof(scoreValue));
+// console.log("type of myFunction = "+typeof(myFunction));
+
+console.log(`\n type of imagniary number = ${typeof(imaginaryNumber)} \n`);
+console.log(`type of bigNumber = ${typeof(bigNumber)} \n`);
+console.log(`type of scoreValue = ${typeof(scoreValue)} \n`);
+console.log(`type of scoreValue = ${typeof(myFunction)} \n`);
+
+
